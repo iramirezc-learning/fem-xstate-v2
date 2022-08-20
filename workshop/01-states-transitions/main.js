db159a9e3208e9591b65a77944e9b312a653d7a2
Isaac Ramirez <iramirezc@live.com.mx>
2022-08-20 18:54:15 -0500

    Solve State Transition Exercise.

--- workshop/01-states-transitions/main.js
+++ workshop/01-states-transitions/main.js
@@ -1,31 +1,53 @@
 // @ts-check
-import '../style.css';
-import { createMachine, assign, interpret, send } from 'xstate';
-import elements from '../utils/elements';
+import "../style.css";
+import { createMachine, assign, interpret, send } from "xstate";
+import elements from "../utils/elements";
 
-import { inspect } from '@xstate/inspect';
+import { inspect } from "@xstate/inspect";
 
 // inspect({
 //   iframe: false,
 //   url: 'https://stately.ai/viz?inspect',
 // });
 
-const playerMachine = createMachine({});
+const playerMachine = createMachine({
+  initial: "loading",
+  states: {
+    loading: {
+      on: {
+        LOADED: "playing",
+      },
+    },
+    playing: {
+      on: {
+        PAUSE: "paused",
+      },
+    },
+    paused: {
+      on: {
+        PLAY: "playing",
+      },
+    },
+  },
+});
 
 const service = interpret(playerMachine, { devTools: true }).start();
 
-elements.elPlayButton.addEventListener('click', () => {
-  service.send({ type: 'PLAY' });
+elements.elPlayButton?.addEventListener("click", () => {
+  service.send({ type: "PLAY" });
 });
-elements.elPauseButton.addEventListener('click', () => {
-  service.send({ type: 'PAUSE' });
+elements.elPauseButton?.addEventListener("click", () => {
+  service.send({ type: "PAUSE" });
 });
 
 service.subscribe((state) => {
   console.log(state);
-  elements.elLoadingButton.hidden = !state.matches('loading');
-  elements.elPlayButton.hidden = !state.can({ type: 'PLAY' });
-  elements.elPauseButton.hidden = !state.can({ type: 'PAUSE' });
+  // @ts-ignore
+  elements.elLoadingButton.hidden = !state.matches("loading");
+  // @ts-ignore
+  elements.elPlayButton.hidden = !state.can({ type: "PLAY" });
+  // @ts-ignore
+  elements.elPauseButton.hidden = !state.can({ type: "PAUSE" });
 });
 
-service.send({ type: 'LOADED' });
+service.send({ type: "LOADED" });
